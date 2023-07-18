e717d471e4e800a102c25feeda2a812735ac4fc0
Dominic Kudom <dominickudom@icloud.com>
2023-07-18 06:16:24 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,7 +3,7 @@
  * _isalpha - Checks for alphabetic character
  * @c: The character to be checked
  * Return: 1 for alphabetic character or 0 for anything else
- */
+*/
 int _isalpha(int c)
 {
 if ((c >= 65 && c <= 97) || (c >= 97 && c <= 122))
@@ -11,4 +11,5 @@ if ((c >= 65 && c <= 97) || (c >= 97 && c <= 122))
 return (1);
 }
 return (0);
-} 
+}
+
