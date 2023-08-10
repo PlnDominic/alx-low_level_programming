df08914cde77980864cc2ae17ed17ef60951ba95
Dominic Kudom <dominickudom1738@gmail.com>
2023-08-10 13:12:48 +0000

    0 malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -8,11 +8,11 @@
  */
 void *malloc_checked(unsigned int b)
 {
-	void *ptr;
+    void *ptr;
 
-	ptr = malloc(b);
+    ptr = malloc(b);
 
-	if (ptr == NULL)
-		exit(98);
-	return (ptr);
+    if (ptr == NULL)
+        exit(98);
+    return (ptr);
 }
