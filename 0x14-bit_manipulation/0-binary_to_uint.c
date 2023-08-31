47adc859dd8b3d27ac71ddfa7f14ed5b3f482d82
Dominic Kudom <dominickudom1738@gmail.com>
2023-08-31 12:57:57 +0000

    binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,15 +12,15 @@ unsigned int convert_binary_to_uint(const char *binary_string)
     unsigned int decimal_value = 0;
 
     if (!binary_string)
-        return 0;
+        return (0);
 
     for (index = 0; binary_string[index]; index++)
     {
         if (binary_string[index] < '0' || binary_string[index] > '1')
-            return 0;
+            return (0);
 
         decimal_value = 2 * decimal_value + (binary_string[index] - '0');
     }
 
-    return decimal_value;
+    return (decimal_value);
 }
