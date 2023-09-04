5120882ef4c2b7f4af5f22ebb607fec4467a06ca
Dominic Kudom <dominickudom1738@gmail.com>
2023-09-04 16:37:15 +0000

    read

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,43 +1,29 @@
 #include "main.h"
 #include <stdlib.h>
-#include <unistd.h> // Include for open, read, write, and close functions
-#include <fcntl.h>  // Include for file control options
 
 /**
- * read_and_print_textfile - Read text from a file and print it to STDOUT.
- * @filename: The name of the text file to be read.
- * @letters: The number of letters to be read and printed.
- * Return: The actual number of bytes read and printed, or 0 on failure or if filename is NULL.
+ * read_textfile- Read text file print to STDOUT.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w- actual number of bytes read and printed
+ *        0 when function fails or filename is NULL.
  */
-ssize_t read_and_print_textfile(const char *filename, size_t letters)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-    char *buffer;
-    int file_descriptor;
-    ssize_t bytes_read;
-    ssize_t bytes_written;
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
 
-    // Open the file for reading
-    file_descriptor = open(filename, O_RDONLY);
-    if (file_descriptor == -1)
-        return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
 
-    // Allocate memory for a buffer to store the read data
-    buffer = malloc(sizeof(char) * letters);
-    if (buffer == NULL)
-    {
-        close(file_descriptor);
-        return (0);
-    }
-
-    // Read data from the file into the buffer
-    bytes_read = read(file_descriptor, buffer, letters);
-
-    // Write the read data to STDOUT
-    bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-
-    // Free the allocated memory and close the file
-    free(buffer);
-    close(file_descriptor);
-
-    return (bytes_written);
+	free(buf);
+	close(fd);
+	return (w);
 }
+
